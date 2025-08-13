fab34bd32e61f789967e113e2dfeb28fef0af0b3
julioram151005 <jramirezp41@miumg.edu.gt>
2025-08-12 20:07:26 -0600

    Error con el URL

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
+  const express = require("express");
+  const bodyParser = require("body-parser");
+  const cors = require("cors");
+  const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:8081"
-};
+  const corsOptions = {
+    origin: "*"
+  };
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const db = require("./musica/models");
+  const db = require("./musica/models");
 
-db.sequelize.sync()
-  .then(() => {
-    console.log("Base de datos sincronizada.");
-  })
-  .catch(err => {
-    console.error("Error al sincronizar la base de datos:", err.message);
+  db.sequelize.sync()
+    .then(() => {
+      console.log("Base de datos sincronizada.");
+    })
+    .catch(err => {
+      console.error("Error al sincronizar la base de datos:", err.message);
+    });
+
+
+  require("./musica/routes/musica.route")(app);
+
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Bienvenido a la API de la musica." });
   });
 
-
-require("./musica/routes/musica.route")(app);
-
-
-app.get("/", (req, res) => {
-  res.json({ message: "Bienvenido a la API de la musica." });
-});
-
-const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}.`);
-});
+  const PORT = process.env.PORT || 8081;
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}.`);
+  });

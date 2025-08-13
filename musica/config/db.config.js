77a17213cc023aef05a610894fa1eab97d34f5d2
julioram151005 <jramirezp41@miumg.edu.gt>
2025-08-12 19:14:08 -0600

    Base Nube

--- musica/config/db.config.js
+++ musica/config/db.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  HOST: "localhost",
-  PORT: 5432,
-  USER: "postgres",
-  PASSWORD: "1234",
-  DB: "postgres",
+  HOST: "ep-morning-poetry-a4jscw3c-pooler.us-east-1.aws.neon.tech",
+  PORT: 5432, // Puerto por defecto de PostgreSQL
+  USER: "neondb_owner",
+  PASSWORD: "npg_NM0wjqAJ3ZIH",
+  DB: "neondb",
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // necesario para conexiones SSL en Neon
+    }
+  },
   pool: {
     max: 5,
     min: 0,
